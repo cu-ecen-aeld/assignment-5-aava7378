1f33b063133b24fbaf7efc18fde181a0d401cdf3
aava7378 <aava7378@Ubuntu.myguest.virtualbox.org>
2026-02-15 16:47:58 -0700

    Buildroot: build/install aesdsocket and init script

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = db0857cbb2b6bb299d77c23ec24a81d58ebabaa1
+AESD_ASSIGNMENTS_VERSION = fe35da66906cc770e53401b62019a2ab49ff09a7 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
