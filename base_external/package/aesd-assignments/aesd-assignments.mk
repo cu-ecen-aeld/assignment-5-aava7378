3a7849041a707d938f6c03924c5b87bc9e5c6eaf
aava7378 <aava7378@Ubuntu.myguest.virtualbox.org>
2026-02-15 19:54:18 -0700

    Update AESD_ASSIGNMENTS_VERSION to latest assignment 3 commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = fe35da66906cc770e53401b62019a2ab49ff09a7 
+AESD_ASSIGNMENTS_VERSION = b2cc3dd977579cb63697259b5c49e5c66cb4ac72 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,15 +15,21 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/writer        $(TARGET_DIR)/usr/bin/writer
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder.sh     $(TARGET_DIR)/usr/bin/finder.sh
+	$(INSTALL) -D -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/finder-test.sh
+
+	$(INSTALL) -D -m 0644 $(@D)/conf/username.txt   $(TARGET_DIR)/etc/finder-app/conf/username.txt
+	$(INSTALL) -D -m 0644 $(@D)/conf/assignment.txt $(TARGET_DIR)/etc/finder-app/conf/assignment.txt
+
 	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdchar_load $(TARGET_DIR)/usr/bin/aesdchar_load
-	$(INSTALL) -D -m 0755 $(@D)/server/aesdchar_unload $(TARGET_DIR)/usr/bin/aesdchar_unload
 
 	$(INSTALL) -D -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
